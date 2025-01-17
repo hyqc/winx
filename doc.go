8141790017ff3f4f5753e4e70635053960d8f826
hyqc <test@qq.com>
2025-01-17 10:47:52 +0800

    增加doc.go

---
+++ doc.go
@@ -0,0 +1,9 @@
+//go:build ignore
+
+// Package winx implements utilities for handling custom data structures.
+//
+// This package provides types and functions for working with MyType objects,
+// which represent complex data in an efficient manner.
+//
+// Example:
+package winx
